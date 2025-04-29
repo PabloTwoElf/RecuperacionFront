3417b275a2c6e1e5424c54bc7028994fd300d445
PabloCriolloEscobar <130104039+PabloTwoElf@users.noreply.github.com>
2025-04-28 23:08:53 -0500

    FrontDelServidor

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -1,19 +1,22 @@
 package com.example.restservice;
 
 import java.util.concurrent.atomic.AtomicLong;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")  // Habilitar CORS para todo el controlador
 @RestController
 public class GreetingController {
 
-	private static final String template = "Hello, %s!";
-	private final AtomicLong counter = new AtomicLong();
+	private static final String template = "¡Hola, %s!";  // Mensaje en español
+	private final AtomicLong contador = new AtomicLong();
 
 	@GetMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	public Greeting saludar(
+			@RequestParam(value = "name", defaultValue = "Mundo") String nombre) {
+
+		return new Greeting(contador.incrementAndGet(), String.format(template, nombre));
 	}
 }
